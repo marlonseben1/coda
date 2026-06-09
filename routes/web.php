c4747c50b9e6d763d60ca93cd3c094b46989b4aa
Marlon Seben <marlonseben1@gmail.com>
2026-06-09 01:06:41 -0300

    refactor(login): :recycle: remove tela de welcome e refatora readme
    na rota "/" exibe login para usuário não autenticado e dashboard para quem está autenticado

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\TransacaoController;
 use App\Http\Controllers\TransferenciaController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', fn () => auth()->check()
+    ? redirect()->route('dashboard')
+    : redirect()->route('login')
+)->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
